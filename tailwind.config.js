d6cd0dc94bfe8202e878c985873ea72a462f543f
Abhishek Tiwari <111308235+abhishek-v007@users.noreply.github.com>
2024-04-20 00:02:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#B7C9F2',
+        headerFooterColor: '#9195F6',
+       },
+      }
+  },
+  plugins: [require("daisyui")],
+};
