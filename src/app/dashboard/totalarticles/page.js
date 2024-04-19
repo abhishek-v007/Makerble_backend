d7b7e38fe39b8bae058a4e14f9598340a421dba9
Abhishek Tiwari <111308235+abhishek-v007@users.noreply.github.com>
2024-04-20 00:15:56 +0530

    Add files via upload

---
+++ src/app/dashboard/totalarticles/page.js
@@ -0,0 +1,87 @@
+
+'use client'
+import { useEffect, useState } from 'react';
+import { MdDelete } from 'react-icons/md';
+import { FaEdit } from 'react-icons/fa';
+import SectionTitle from '@/app/shared components/SectionTitle';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+const TotalArticles = () => {
+  const router = useRouter();
+  const [posts, setPosts] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch(`${apiUrl}/api/post`, {
+        cache: 'no-store',
+      });
+
+      if (!res.ok) {
+        throw new Error('Failed to fetch articles');
+      }
+
+      const data = await res.json();
+      setPosts(data);
+    } catch (error) {
+      console.error('Error loading posts: ', error);
+      toast.error('Failed to fetch articles');
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, [posts]); // Fetch data on component mount
+
+  const handleRemovePost = async (id) => {
+    const confirmed = confirm("Are you sure?");
+
+    if (confirmed) {
+      const res = await fetch(`${apiUrl}/api/post?id=${id}`, {
+        method: "DELETE",
+      });
+      
+      if (res.ok) {
+        toast.success('Successfully Deleted Post')
+        router.refresh();
+      }
+    }
+  };
+
+
+  const postsArray = posts?.posts;
+  
+
+  return (
+    <section>
+      <div className="lg:-mt-20">
+        <SectionTitle title={'All Articles'} />
+      </div>
+      {postsArray && postsArray.map((ar, index) => (
+        <div key={ar._id} className="mb-16">
+          <div className="contactForm -mt-10 border-4 border-headerFooterColor rounded px-10 py-4">
+            <div className="flex justify-center gap-1">
+              <p className="text-lg">{index + 1}</p>
+              <p className="text-lg">{ar.topicName}</p>
+            </div>
+            <div className="flex justify-center gap-5">
+              <button onClick={() => handleRemovePost(ar._id)}>
+                <Link href={''} className="hover:font-bold text-xl">
+                  <MdDelete />
+                </Link>
+              </button>
+              <Link href={`/dashboard/totalarticles/${ar._id}`} className="hover:font-bold text-xl">
+                <FaEdit />
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default TotalArticles;
---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Inter, Roboto } from "next/font/google";
+import "./globals.css";
+import Header from "./shared components/Header";
+import Footer from "./shared components/Footer";
+import Toaster from '@/app/shared components/Toaster'
+import AuthProvider from "@/authentication/AuthProvider";
+
+const inter = Roboto({ subsets: ["latin"] , weight: ['100', '300', '400', '500', '700', '900']});
+
+export const metadata = {
+  title: "Sadik Hasan Shuvo",
+  description: "Assistant Professor at Jatiya Kabi Kazi Nazrul Islam University",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          <Header/>
+          <main className="max-w-5xl mx-auto min-h-[270px]   px-10 lg:px-0">{children}</main>
+          <Footer/>
+        </AuthProvider>
+        <Toaster toastOptions={{
+        style: {
+          background: '#cbd5e1',
+          color: '#3C3C59'
+        }
+      }}/>
+
+      </body>
+    </html>
+  );
+}
